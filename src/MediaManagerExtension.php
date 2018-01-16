fa904fb53af80225a08569d3eca00f31f38de7ee
Cory <cory@corydowdy.com>
2018-01-16 11:30:54 -0500

    add skeleton extension class

---
+++ src/MediaManagerExtension.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bolt\Extension\cdowdy\mediamanager;
+
+use Bolt\Asset\File\JavaScript;
+use Bolt\Asset\File\Stylesheet;
+use Bolt\Controller\Zone;
+use Bolt\Extension\SimpleExtension;
+use Bolt\Menu\MenuEntry;
+
+class MediaManagerExtension extends SimpleExtension
+{
+
+}
